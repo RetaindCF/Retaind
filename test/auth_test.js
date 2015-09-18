6285234bccdd500581ced9012f5f76adb35b8662
Erik Luetkehans <eluetekhans@gmail.com>
2015-09-18 09:59:58 -0700

    set up auth_test skeleton

--- test/auth_test.js
+++ test/auth_test.js
@@ -4,3 +4,9 @@ var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
 
 require(__dirname + "/../server.js");
+
+describe('Travis test', function() {
+  it('should work on travis', function() {
+    expect(true).to.eql(true);
+  });
+});
