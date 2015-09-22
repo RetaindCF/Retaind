3a6e1f72e0d18ad09071735bed4efc3d37221cb1
David Sheffield <david.sheffield21@gmail.com>
2015-09-21 21:30:53 -0700

    gulp file

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var gulpMocha = require('gulp-mocha');
+
+gulp.task('jshint', function() {
+  return gulp.src(['index.js', 'server.js', 'lib/**/*.js', 'models/**/*.js'])
+  return gulp.src(['routes/**/*.js', 'test/**/*.js', 'util/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+  return gulp.src('test/**/*.js')
+    .pipe(gulpMocha({reporter: 'nyan'}));
+});
+
+gulp.task('default', ['jshint', 'test']);
