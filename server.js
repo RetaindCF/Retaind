3b949ce3d20cf14f12541ebfe915273658fe9102
David Sheffield <david.sheffield21@gmail.com>
2015-09-22 13:36:39 -0700

    removing extra space

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-
 var jsonParser = require('body-parser').json();
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/db');
