6f43931c5d042d8e70199465ec773f270a4dd5cb
Erik Luetkehans <eluetekhans@gmail.com>
2015-09-24 16:11:01 -0700

    fixed daily email to go daily

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ setInterval(function() {
   // Checks due date of ambitions and sends an email if they are over due
   dateCheck();
   console.log('daily emails sent');
-}, 1000 * 60 );
+}, 1000 * 60 * 60 * 24);
 
 
 app.listen(port, function() {
