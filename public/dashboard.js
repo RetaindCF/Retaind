40f1828e9e2f20936358bdf0f82b0fadf956900c
Duncan <Duncan.David@gmail.com>
2015-09-24 17:35:41 -0700

    rudimentary ui, pulling info from db

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,5 +1,49 @@
-$(document).ready(function(){
- var userLocalToken = localStorage.getItem("token")
+var noteCount = 0;
+
+/*<form name="myForm" id="testForm" method="POST" action="send.php">
+UserName: <input type="text" name="user" value="test" /> <br/>
+Password: <input type="password" name="password" value="test"/> <br/>
+</form> */
+
+
+function noteMake2(dborform, color) {
+  var setColor = color || "yellow";
+  var noteId = "note" + noteCount;
+  var noteFormId = "noteform" + noteCount;
+  dborform = dborform || {activity: "A new ambition...", dueDate: "set a due date"};
+  $("<div>").attr("id", "note" + noteCount).addClass("box").addClass(setColor).addClass("quote-container")
+  .appendTo("main");
+  $("<h2>").appendTo("#noteId");
+  $("<form>").addClass("activity").attr("id", "noteform" + noteCount).appendTo("#" + noteId);
+  $('<input type="text">').val(dborform.ambition || "Enter Text")
+  .addClass(color).addClass("activity").appendTo("#" + noteFormId);
+  $('<input type="submit">').val("Submit Changes").appendTo("#" + noteFormId);
+   noteCount++;
+}
+
+function noteMake(dborform, color){
+  color = color || "yellow";
+  dborform = dborform || {activity: "A new ambition...", dueDate: "set a due date"};
+  $("<div>").attr("id", "note" + noteCount).addClass("box").addClass(color).addClass("quote-container").appendTo("main");
+  $("<button>").addClass("editremind").text("edit").appendTo(".box:last");
+  $("<button>").text("cancel").addClass("cancel").appendTo(".box:last").hide();
+  $("<form>").addClass("remindform").attr('method', 'POST').appendTo(".box:last");
+  var $remind = $(".remindform:last");
+  var $input = $('<input type="text" class="activity">');
+  $input.val(dborform.ambition);
+  $input.appendTo($remind);
+  $remind.append($input);
+  $remind.append("<br>");
+  $remind.append("<br>");
+  $remind.append('<input type="submit" value="Submit Changes">');
+  $remind.append('<p class="lastlogin"></p>');
+  $(".lastlogin").text(dborform.dueDate);
+  $(".activity").attr('disabled', true);
+}
+
+
+$(document).ready(function() {
+ var userLocalToken = localStorage.getItem("token");
  console.log(userLocalToken);
  var token = JSON.parse(userLocalToken).token;
  console.log("parsed token", token);
@@ -18,16 +62,23 @@ $(document).ready(function(){
     success    : function(data){
       console.log("data", data);
       curAmbs = data;
+      curAmbs.forEach(function(item){
+        noteMake2(item, "yellow");
+        boxSubmitUpdate();
+      });
     }
   });
 
   $("<button>").addClass("newremind").text("new reminder").appendTo("body");
 
   $(".newremind").on("click", function() {
-    $("<div>").addClass("box").appendTo("main");
+    noteMake2(null, "blue");
+    boxSubmitUpdate();
+  });
+   /* $("<div>").attr("id", "note" + noteCount).addClass("box").addClass("blue").addClass("quote-container").appendTo("main");
     $("<button>").addClass("editremind").text("edit").appendTo(".box:last");
     $("<button>").text("cancel").addClass("cancel").appendTo(".box:last").hide();
-    $("<form>").addClass("remindform").attr('method', 'PUT').appendTo(".box:last");
+    $("<form>").addClass("remindform").attr('method', 'POST').appendTo(".box:last");
     $(".remindform:last").append('activity: <input type="text" class="activity">')
     $(".remindform:last").append("<br>");
     $(".remindform:last").append("<br>");
@@ -36,34 +87,13 @@ $(document).ready(function(){
     $(".activity").attr('disabled', true);
     editUpdate();
     boxSubmitUpdate();
-  })
-
-  $(".activity").attr('disabled', true);
-  $("<button>").addClass("cancel").text("cancel").prependTo("#box1");
-  $(".cancel").hide();
-
-function editUpdate() {
-  $(".editremind").on("click", function(e){
-    e.preventDefault();
-    $(".activity").attr('disabled', false);
-    $(".editreminder").toggle();
-    $(".cancel").toggle();
-  })
-
-  $(".cancel").on("click", function(){
-    $(".activity").val("");
-    $(".activity").attr('disabled', true);
-    $(this).toggle();
-    $(".editreminder").toggle();
-  })
-}
+  })*/
 
   function boxSubmitUpdate(){
     $( ".box").submit(function(e) {
-      $(".lastlogin").text("last checkin: " + new Date().toString())
       e.preventDefault();
-      var activity = $(".activity").val();
-      $(".activity").attr('disabled', true);
+      var activity = $(this).find("input").val();
+      console.log(activity);
       var j = JSON.stringify({username: username, token: token, ambitions: activity});
       //console.log(j);
       $.ajax({
@@ -78,4 +108,5 @@ function editUpdate() {
       });
     });
   }
+
 }); //document ready end
